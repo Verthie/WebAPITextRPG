2778a7df775471946dac11ced9e2ec40e9dd0113
Verth <vertisixs@gmail.com>
2023-01-20 20:12:09 +0100

    Returning characters
    Models:
    Added Character
    Added Character Classes
    Controllers:
    List of characters creation
    Http Requests \/
    Added GET method for returning a list of characters
    Added GET method for returning a single character using the id parameter

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITextRPG.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> //creating a list of characters
+        {
+            new Character(),
+            new Character { Id = 1, Name = "Lunk"} //adding a character named "Lunk" with an id = 1 and the rest are default values
+        };
+
+        [HttpGet("GetAll")] //Get method returning a list of all characters
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")] //Get method returnig a single character using the id parameter
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id)); //Returns the first character where the id of the characters equals the given ID
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPITextRPG.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Gandalf"; //Character Name - default is "Gandalf"
+        public int HitPoints { get; set; } = 100; //Character HitPoints - default is 100
+        public int Strength { get; set; } = 10; //Character Strength - default is 10
+        public int Defense { get; set; } = 10; //Character Defense - default is 10
+        public int Intelligence { get; set; } = 10; //Character Intelligence - default is 10
+        public RpgClass Class { get; set; } = RpgClass.Mage; //Character Class - default is Mage (List of all classes is in RpgClass.cs in models)
+    }
+}
